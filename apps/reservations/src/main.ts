7a5a3d3e4c14ff986aaca8231fb839d42f1d1150
silviuvijiala <silviuvaj@yahoo.com>
2024-12-14 15:14:38 +0200

    New docs for reservations app

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -3,12 +3,32 @@ import { ReservationsModule } from './reservations.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
 
+/**
+ * The `bootstrap` function initializes and starts the NestJS application.
+ */
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
 
+  /**
+   * Use global pipes for validation.
+   * - `ValidationPipe`: Automatically validates incoming requests against the defined DTOs.
+   * - Options:
+   *    - `whitelist: true`: Automatically strips properties that are not defined in the DTO, improving security.
+   */
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  /**
+   * Integrates the Pino Logger into the application.
+   * - Retrieves the logger instance from the application context and sets it as the global logger.
+   * - Provides structured and high-performance logging for the application.
+   */
   app.useLogger(app.get(Logger));
 
+  /**
+   * Start the application and listen on the specified port.
+   * - `process.env.port`: Retrieves the port from environment variables.
+   * - Defaults to `3000` if no port is specified.
+   */
   await app.listen(process.env.port ?? 3000);
 }
 bootstrap();
--- apps/reservations/src/reservations/dto/create-reservation.dto.ts
+++ apps/reservations/src/reservations/dto/create-reservation.dto.ts
@@ -1,19 +1,45 @@
 import { Type } from 'class-transformer';
 import { IsDate, IsNotEmpty, IsString } from 'class-validator';
 
+/**
+ * CreateReservationDto is a Data Transfer Object (DTO) that defines the structure
+ * and validation rules for creating a reservation.
+ */
 export class CreateReservationDto {
+  /**
+   * The start date of the reservation.
+   * - Must be a valid Date object.
+   * - Automatically transformed into a Date using `class-transformer`.
+   * @type {Date}
+   */
   @IsDate()
   @Type(() => Date)
   startDate: Date;
 
+  /**
+   * The end date of the reservation.
+   * - Must be a valid Date object.
+   * - Automatically transformed into a Date using `class-transformer`.
+   * @type {Date}
+   */
   @IsDate()
   @Type(() => Date)
   endDate: Date;
 
+  /**
+   * The ID of the place being reserved.
+   * - Must be a non-empty string.
+   * @type {string}
+   */
   @IsString()
   @IsNotEmpty()
   placeId: string;
 
+  /**
+   * The ID of the invoice associated with the reservation.
+   * - Must be a non-empty string.
+   * @type {string}
+   */
   @IsString()
   @IsNotEmpty()
   invoiceId: string;
